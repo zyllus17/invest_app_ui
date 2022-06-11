39fe058e4aec764e5bb9b5e0c02f4bbef0264517
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-06-11 17:16:27 +0530

    completed

---
+++ lib/constants/strings.dart
@@ -0,0 +1,35 @@
+class Strings {
+  Strings._();
+
+  static const String hey = "Hey, Maruf";
+  static const String portfolio = "Your portfolio";
+  static const String money = "Your portfolio";
+  static const String myAssets = "My Assets";
+  static const String value = "¥ 25,055";
+  static const String popularCurreny = "Popular Currency";
+  static const String viewAll = "View All";
+  static const String euro = "Euro";
+  static const String euroReturn = "6% return";
+  static const String yen = "Yen";
+  static const String yenReturn = "8% return";
+  static const String learnAboutInvestment = "Learn about Investment";
+  static const String investInStock = "Investing in Stocks";
+  static const String investInStockSubtitle =
+      "2020 has ssen some of the most historic rise in stock market since the Dot com bubble";
+  static const String investInForex = "Investing in Forex";
+  static const String investInForexSubtitle =
+      "Forex market is at an all time high. The quantitative easing will have far reaching consequences";
+  static const String investInBonds = "Investing in Bonds";
+  static const String investInBondsSubtitle =
+      "Bonds are still the safest funds available in the market. With the countries in economics ruin, it sure can give a good yield";
+  static const String totalAsset = "Your total asset portfolio";
+  static const String currentPlans = "Current Plans";
+  static const String history = "History";
+  static const String euroShare = 'Sold "Euro"';
+  static const String euroCost = '¥ 5,000';
+  static const String yenShare = 'Bought "Yen"';
+  static const String yenCost = '¥ 8,000';
+  static const String rupeeShare = 'Bought "Rupee"';
+  static const String rupeeCost = '¥ 6,000';
+  static const String date = '12 May 2021';
+}
